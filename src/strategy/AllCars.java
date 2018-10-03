4ecb1822b0e43a6f09de88b1fc3b718ef31bfd44
Liam McGovern <Liam.McGovern@students.ittralee.ie>
2018-10-03 15:51:27 +0100

    Added AllCars.java to project to display all cars uncategorized of body shape

---
+++ src/strategy/AllCars.java
@@ -0,0 +1,12 @@
+import strategy.Vehicle;
+
+public abstract class AllCars extends Vehicle {
+    public void allCars() {
+
+        System.out.println("A List of All Cars For Sale:");
+
+    }
+
+    @Override
+    public void
+}
