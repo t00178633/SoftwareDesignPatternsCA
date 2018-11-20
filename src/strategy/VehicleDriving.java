b845df871409803e3bf340f2c6f5f30b1de67ae6
t00178633 <liam.mcgovern@students.ittralee.ie>
2018-11-20 22:22:49 +0000

    Updated VehicleDriving println

--- src/strategy/VehicleDriving.java
+++ src/strategy/VehicleDriving.java
@@ -2,6 +2,6 @@ package strategy;
 
 public class VehicleDriving implements Driveability {
 	public void driveable() {
-		System.out.println("Car is fully functional and driveable");
+		System.out.println("Car is fully functional and Driveable");
 	}
 }
